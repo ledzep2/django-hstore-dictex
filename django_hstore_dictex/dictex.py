6f976b8952d3cb1255f26b125436b645cbb1d030
Russell <yufeiwu@gmail.com>
2012-12-06 10:42:26 +0800

    Fix

--- django_hstore_dictex/dictex.py
+++ django_hstore_dictex/dictex.py
@@ -1,5 +1,5 @@
 from django_hstore.hstore import DictionaryField
-from django_jsonformfieldex.field import JSONFormFieldEx
+from jsonformfieldex.field import JSONFormFieldEx
 
 class DictionaryFieldEx(DictionaryField):
     def __init__(self, *args, **kwargs):
@@ -9,10 +9,11 @@ class DictionaryFieldEx(DictionaryField):
         super(DictionaryFieldEx, self).__init__(self, *args, **kwargs)
 
     def formfield(self, **kwargs):
-        if "form_class" not in kwargs:
-            kwargs["form_class"] = JSONFormFieldEx
+        #if "form_class" not in kwargs:
+        #    kwargs["form_class"] = JSONFormFieldEx
 
         kwargs['fields'] = self.fields
         kwargs['allow_json_input'] = self.allow_json_input
 
-        return super(DictionaryField, self).formfield(**kwargs)
+        #return super(DictionaryFieldEx, self).formfield(**kwargs)
+        return JSONFormFieldEx(**kwargs)
--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 setup(name="django-hstore-dictex",
-      author="Russell",
-      author_email="yufeiwu@gmail.com",
-      packages=['django_hstore_dictex'],
-      description='An enhanced dictionary field with form support of django-hstore',
-      long_description=open('README.md').read(),
-      )
+    version="0.9",
+    author="Russell",
+    author_email="yufeiwu@gmail.com",
+    packages=['django_hstore_dictex'],
+    description='An enhanced dictionary field with form support of django-hstore',
+    long_description=open('README.md').read(),
+)
 
